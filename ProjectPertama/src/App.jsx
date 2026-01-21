881714b9a2ad944d82a596a42d5bd3529f3ed9bc
rofiifagustina <piip.opiip1729@gmail.com>
2026-01-21 10:15:04 +0700

    add componen css

--- ProjectPertama/src/App.jsx
+++ ProjectPertama/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Greeting() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Halo,Selamat Pagi</h1>
+      <p>SEMANGAT YA </p>
     </>
-  )
+  );
+}
+//props/properti
+function Biodata(props) {
+  return (
+    <div>
+      <p>Nama : {props.nama}</p>
+      <p>Kelas : {props.kelas}</p>
+      <p>Nisn :{props.Nisn}</p>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Greeting />
+      <Biodata nama="Rofiif" kelas="XI PPLG 1" Nisn="21652" />
+      <Biodata nama="Budi" kelas="XI MPLB 2" Nisn="21671"/>
+      <Biodata nama="Siti" kelas="X AKL 3"Nisn="87651"/>
+    </>
+  );
+}
+
+export default App;
